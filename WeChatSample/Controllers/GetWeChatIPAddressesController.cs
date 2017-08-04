ba6a1d2ec0cb4e9a327a0310bfe9c8c32d53122e
David Zhao (DEVDIV) <dazhao@microsoft.com>
2017-08-03 20:30:31 -0700

    Test access token by retrieving wechat ip addresses

---
+++ WeChatSample/Controllers/GetWeChatIPAddressesController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WeChatSample.Authorization;
+
+namespace WeChatSample.Controllers
+{
+    [Route("api/[controller]")]
+    public class GetWeChatIPAddressesController : Controller
+    {
+        private readonly IAccessTokenProvider _accessTokenProvider;
+
+        public GetWeChatIPAddressesController(IAccessTokenProvider accessTokenProvider)
+        {
+            _accessTokenProvider = accessTokenProvider;
+        }
+
+        [HttpGet]
+        public async Task<string> Get()
+        {
+            string accessToken = await _accessTokenProvider.GetAccessTokenAsync(CancellationToken.None);
+
+            using (var client = new HttpClient())
+            {
+                string url = FormattableString.Invariant($"https://api.weixin.qq.com/cgi-bin/getcallbackip?access_token={accessToken}");
+
+                var response = await client.GetAsync(url);
+
+                return await response.Content.ReadAsStringAsync();
+            }
+        }
+    }
+}
